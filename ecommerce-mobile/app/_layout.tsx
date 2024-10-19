7431e39654b2123e9ead24fea8a1e605829f038d
Gabriel Paez <gabpaez@gmail.com>
2024-10-19 01:54:17 -0300

    setup expo router

---
+++ ecommerce-mobile/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack, Tabs } from 'expo-router'
+
+export default function Rootlayout() {
+    return (
+        <Stack />
+  )
+}
